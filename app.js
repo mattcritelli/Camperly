18ae8e8c07ebd38818a1e4872585abb439310a47
Matt C <matthew.critelli@gmail.com>
2017-08-31 15:26:53 -0600

    Add body-parser dependency.  Add routes/forms to create new campground.  Add basic links to navigate between pages.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
 var campgrounds = [
   {name:"Bear Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
   {name:"Morraine Hill", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
@@ -7,6 +8,8 @@ var campgrounds = [
 ]
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res){
   res.render("landing")
@@ -16,6 +19,18 @@ app.get("/campgrounds", function(req, res){
   res.render("campgrounds", {campgrounds: campgrounds});
 })
 
+app.post("/campgrounds", function(req,res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { "name": name, "image": image }
+  campgrounds.push(newCampground);
+  res.redirect("campgrounds")
+})
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
+})
+
 app.listen(3000, function(){
-  console.log("Winter is coming... on port 3000.")
+  console.log("Winter is coming... on port 3000.");
 })
