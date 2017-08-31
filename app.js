dc89c94964a0fc7fed2099e8b202be56aa1d1d73
Matt C <matthew.critelli@gmail.com>
2017-08-31 14:36:30 -0600

    Add ejs view engine. Add landing page and campgrounds test pages and routes. Add partials directory with header/footer and incorporate into landing page.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var app = express();
+var campgrounds = [
+  {name:"Bear Creek", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+  {name:"Morraine Hill", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
+  {name:"Great Big Elk Campground", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
+]
+
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-  res.send("Future landing page...")
+  res.render("landing")
+})
+
+app.get("/campgrounds", function(req, res){
+  res.render("campgrounds", {campgrounds: campgrounds});
 })
 
 app.listen(3000, function(){
